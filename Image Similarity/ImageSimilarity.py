c9ee2ee23f536ffcde85957779507c1a7ab30aaf
Pavith Dinusara Bambaravanage <160596797+Pavith19@users.noreply.github.com>
2024-07-23 17:35:13 +0530

    Add files via upload

---
+++ Image Similarity/ImageSimilarity.py
@@ -0,0 +1,36 @@
+def checkSimilar(a,b):
+    count=0
+    c=min(len(a[0]),len(b[0]))
+    r=min(len(a),len(b))
+    for i in c:
+        for j in r:
+            if a[i,j]=b[i,j]:
+                cout+=1
+    return count
+
+def howSimilar(imgIn, imgRef):
+    similarity = []
+    for 
+
+def turn90(A):
+    rows=len(A)
+    cols=len(A[0])
+    for x in range(rows):
+    for y in range(cols):
+        A2[y][rows - x - 1] = A[x][y]
+    return A2
+       
+def flipH
+
+def matrixflip(m,d):
+    m = myl
+    myl = np.array([[1, 2], [3, 4]])
+    if d=='v': 
+        return np.flip(contour, axis=0)
+    elif d=='h':
+        return np.flip(contour, axis=1)
+
+import  numpy as np
+a = np.array([[1,2,3],[4,5,6],[7,8,9]])
+
+
